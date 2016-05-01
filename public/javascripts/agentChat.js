e8678611fe4771d566ba271f59bf973abfe821e1
Brave <BraveWangDev@126.com>
2016-05-01 21:40:56 +0800

    修改三端容易混淆和命名不清晰的字段名，添加必要的Log,添加流程标记，优化代码结构，并测试

--- public/javascripts/agentChat.js
+++ public/javascripts/agentChat.js
@@ -10,16 +10,18 @@ $(document).ready(function(){
   	//获取客户端IO连接实例
   	var socket = io.connect();
 	console.log('用户端连接IO完成');
+	
 	/**
-	 * 注册用户端登陆点击事件
+	 * 用户登陆
 	 */
 	$('#agentLogin').click(function () {
-		console.log('发送客服登录信息');
-		var agentInfo = {userId : '1001', nikeName : '测试用户1001', channel : '测试App-用户端'};
+		console.log("-------------------------------- 发送用户登录信息 -----------------------------------");
+		//TODO 登录信息来自页面输入（或赋值）
+		var agentInfo = {agentId : '1001', agentNickName : '测试用户1001', channel : '测试App-用户端'};
 		socket.emit('agentLogin', agentInfo);
 		//保存自己的信息
-		socket.userId = agentInfo.userId;
-		socket.nikeName = agentInfo.nikeName;
+		socket.agentId = agentInfo.agentId;
+		socket.agentNickName = agentInfo.agentNickName;
 		socket.channel = agentInfo.channel;
 		console.log('用户端保存用户信息到Socket完成');
 	});
@@ -28,40 +30,52 @@ $(document).ready(function(){
 	 * 接收登陆结果
 	 */
 	socket.on('agentLoginResult',function(data){
-
-		console.log('用户登陆成功 serviceId = ' + data.serviceId + ', nickName = ' + data.nickName);
-		//保存分配客服信息和会话号
-		socket.room = data.serviceId;
-		socket.serviceId = data.serviceId;
-		socket.serviceNickName= data.nickName;
-		$('#agentLoginReturn').val('用户:' + socket.nikeName + ', 登陆成功, 进入会话 : ' +  socket.room);
+		console.log('--------------------------- 收到用户登录结果 ------------------------------');
+		console.log('登录返回 Json = ' + JSON.stringify(data));
+		if(data.resCode == 0){
+			console.log('用户登陆成功 分配serviceId = ' + data.serviceId + ', nickName = ' + data.serviceNickName);
+			//TODO 保存分配客服信息和会话号
+			socket.room = data.serviceId;
+			socket.serviceId = data.serviceId;
+			socket.serviceNickName= data.nickName;
+			$('#agentLoginReturn').val('用户:' + socket.agentNickName + ', 登陆成功, 进入会话 : ' +  socket.room);
+		}else{
+			alert(data.message);
+		}
 	});
 
-	//收到客服端的默认回复
+	/**
+	 * 收到客服端的默认回复
+	 */
 	socket.on('defRespondToAgent', function (data) {
+		console.log('--------------------------- 收到客服端的默认回复 ------------------------------');
 		console.log('收到客服端的默认回复, from = ' + data.from + " , to = " + data.to + " , 消息 = " + data.message);
-		//更新UI
 		$('#defRespondToAgent').val('收到客服端的默认回复, from = ' + data.from + " , to = " + data.to + " , 消息 = " + data.message);
+		//TODO 更新UI - 显示区域内容显示
 	});
 
 	/**
 	 * 用户发送消息给客服
 	 */
 	$('#sendMsgBtn').click(function () {
-		console.log('发送消息给客服');
+		console.log('--------------------------- 发送消息给客服 ------------------------------');
 		//发送给客服
 		var sendMsg = {};
-		sendMsg.from = socket.userId;
+		sendMsg.from = socket.agentId;
 		sendMsg.to= socket.serviceId;
 		sendMsg.message = $('#sendMsg').val();
 		console.log('向客服发送消息 sendMsg.from = ' + sendMsg.from + " , sendMsg.to = " + sendMsg.to + " , sendMsg.message = " + sendMsg.message);
 		socket.emit('userToServiceMsg', sendMsg);
+		//TODO 更新UI - 显示区域内容显示   清空输入框
 	});
 
-	//收到客服端的默认回复
+	/**
+	 * 收到客服端的默认回复
+	 */
 	socket.on('userToServiceMsg', function (data) {
+		console.log('--------------------------- 收到客服端的默认回复 ------------------------------');
 		console.log('收到客服端的默认回复, from = ' + data.from + " , to = " + data.to + " , 消息 = " + data.message);
-		//更新UI
+		//TODO 更新页面显示 - 更新对话区域内容
 		$('#reciverMsg').val(JSON.stringify(data));
 	});
 
@@ -72,33 +86,30 @@ $(document).ready(function(){
 	 * parameter: 消息内容(发送方 接收方 消息内容)
 	 */
 	socket.on('serviceToUserMsg',function(data){
+		console.log('--------------------------- 接收客服回复消息 ------------------------------');
 		console.log('打印当前socket.room = ' + socket.room);//服务器是不会返回并更新本地socket对象的
-		console.log('收到客服消息 data.from = ' + data.from + " , data.to = " + data.to + " , data.message = " + data.message);
-		//更新UI
+		console.log('收到客服回复; data.from = ' + data.from + " , data.to = " + data.to + " , data.message = " + data.message);
+		//TODO 更新页面显示 - 更新对话区域内容
 		$('#reciverMsg').val(JSON.stringify(data));
-		////TODO 显示页面更新UI
-		////构建测试数据
-		//data.from = '1001';
-		//data.to= '001';
-		//data.message = '用户:我需要咨询问题';
-		//console.log('向客服发送消息 data.from = ' + data.from + " , data.to = " + data.to + " , data.message = " + data.message);
-		////用户发送
-		//socket.emit('userToServiceMsg', data);
 	});
 
 	/**
-	 * 用户退出事件
+	 * 用户退出
 	 */
 	$('#agentLogout').click(function () {
-		console.log('点击用户退出 userId = ' + socket.userId + ", nickName = " + socket.nikeName);
+		console.log('--------------------------- 用户退出 ------------------------------');
+		console.log('点击用户退出 agentId = ' + socket.agentId + ", nickName = " + socket.agentNickName);
 		//发送给客服
 		var sendMsg = {};
-		sendMsg.userId = socket.userId;
-		sendMsg.nickName = socket.nikeName;
+		sendMsg.agentId = socket.agentId;
+		sendMsg.agentNickName = socket.agentNickName;
 		socket.emit('agentLogout', sendMsg);
+		//TODO 关闭页面， 清理个人信息缓存
 	});
 
-	//来自服务器的消息 -- 关闭浏览器，主动断开，掉线等任何断开连接的情况
+	/**
+	 * 来自服务器的消息 -- 关闭浏览器，主动断开，掉线等任何断开连接的情况
+	 */
 	socket.on('disconnect',function(){
 		console.log("服务器断开连接");
 	});
